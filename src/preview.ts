atsushieno/vscode-language-review

commit 0782dc0c5488e54352472e5190fcd22b208d47e7

Support nested outline

src/preview.ts
@@ -27,7 +27,7 @@ class ReviewTextDocumentContentProvider implements vscode.TextDocumentContentPro
 		});
 	}
 
-	public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.SymbolInformation[] | Thenable<vscode.SymbolInformation[]> {
+	public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.SymbolInformation[] | Thenable<vscode.DocumentSymbol[]> {
 		return processDocument (document).then (book => document_symbols);
 	}
 
@@ -91,7 +91,13 @@ function showPreview (uri: vscode.Uri) {
 	return vscode.commands.executeCommand ('vscode.previewHtml', getSpecialSchemeUri (uri), vscode.ViewColumn.Two);
 }
 
-var document_symbols: vscode.SymbolInformation[] = Array.of<vscode.SymbolInformation> ();
+var document_symbols: vscode.DocumentSymbol[] = Array.of<vscode.DocumentSymbol> ();
+
+interface ReviewSymbol {
+	readonly level: number;
+	readonly parent: ReviewSymbol | undefined;
+	readonly children: vscode.DocumentSymbol[]
+}
 
 function processDocument (document: vscode.TextDocument): Promise<review.Book> {
 	return review.start (controller => {
@@ -110,16 +116,55 @@ function processDocument (document: vscode.TextDocument): Promise<review.Book> {
 			listener: {
 				// onAcceptables: ... ,
 				onSymbols: function (symbols) {
-					document_symbols = symbols.map<vscode.SymbolInformation> ((src, idx, arr) => {
+					function organizeSymbols (parent: ReviewSymbol, symbols: review.Symbol[]) {
+						if (!symbols.length) {
+							return;
+						}
+
+						let symbol = symbols[0];
+						let docSymbol = new vscode.DocumentSymbol (
+							getLabelName (symbol), "", vscode.SymbolKind.Null, locationToRange (symbol.node.location), locationToRange (symbol.node.location));
+						let level = extractLevel (symbol);
+						if (docSymbol === undefined || level === -Infinity) {
+							return;
+						}
+						docSymbol.children = [];
+						while (parent && level <= parent.level) {
+							parent = parent.parent!;
+						}
+						parent.children.push (docSymbol);
+						organizeSymbols ({level, children: docSymbol.children, parent}, symbols.slice (1));
+					}
+
+					function extractLevel (src: review.Symbol): number {
 						switch (src.symbolName) {
 							case "hd":
-								return new vscode.SymbolInformation (src.labelName, vscode.SymbolKind.Null, locationToRange (src.node.location), document.uri, "Re:View Index");
+								return src.node.toHeadline ().level;
 							case "column":
-								return new vscode.SymbolInformation ("[column] " + src.node.toColumn().headline.caption.childNodes[0].toTextNode().text, vscode.SymbolKind.Null, locationToRange (src.node.location), document.uri, "Re:View Index");
+								return src.node.toColumn ().level;
+							default:
+								return -Infinity;
+						}
+					}
+
+					function getLabelName (src: review.Symbol): string {
+						switch (src.symbolName) {
+							case "hd":
+								return src.labelName;
+							case "column":
+								return "[column] " + src.node.toColumn ().headline.caption.childNodes[0].toTextNode ().text;
 							default:
 								return undefined;
 						}
-					}).filter(ret => ret !== undefined);
+					}
+
+					const root: ReviewSymbol = {
+						level: -Infinity,
+						children: [],
+						parent: undefined
+					};
+					organizeSymbols (root, symbols.filter (o => o.symbolName === "hd" || o.symbolName === "column"));
+					document_symbols = root.children;
 				},
 				onReports: function (reports) {
 					var dc = Array.of<vscode.Diagnostic> ();

commit 3275ad314356c44434cc66df4b29be10664cde68

Fix regression introduced in 0782dc0
 - Didn't consider inline `hd` occurrences

src/preview.ts
@@ -164,7 +164,7 @@ function processDocument (document: vscode.TextDocument): Promise<review.Book> {
 						children: [],
 						parent: undefined
 					};
-					organizeSymbols (root, symbols.filter (o => o.symbolName === "hd" || o.symbolName === "column"));
+					organizeSymbols (root, symbols.filter (o => !o.node.isInlineElement() && o.symbolName === "hd" || o.symbolName === "column"));
 					document_symbols = root.children;
 				},
 				onReports: function (reports) {